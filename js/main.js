528c0b32f028ce6f9086016ba6cbdf2e1067b000
Abbos782 <77330418+Abbos782@users.noreply.github.com>
2021-03-25 17:50:00 +0300

    add

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+let inputTextElemnt = document.querySelector(".text")
+let buttonSendElement = document.querySelector(".send")
+let userElement = document.querySelector(".user")
+
+
+
+
+// buttonSendElement.addEventListener("click", function(){
+
+//       let newMessage = document.createElement("p")
+//       newMessage.innerHTML =inputTextElemnt.value 
+//       userElement.appendChild(newMessage)
+//       inputTextElemnt.value = " "
+    
+ 
+
+// })
+
+
+
+ 
+
+
